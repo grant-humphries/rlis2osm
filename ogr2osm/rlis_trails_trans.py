ab2000067a57e888794ade098041edb228e2312d
grant-humphries <grant.humphries@gmail.com>
2014-12-01 22:50:39 -0800

    cleaned up trails transaltion file

--- ogr2osm/rlis_trails_trans.py
+++ ogr2osm/rlis_trails_trans.py
@@ -1,20 +1,26 @@
+
+# Python Version: 2.7.5
+#--------------------------------
+# In this case the translation file is being used to expand fields (keys) that
+# had to be truncated due to the .dbf spec associated with shapefiles
+
 def filterTags(tags):
 	if tags is None:
 		return
 	newtags = {}
 	for (key, value) in tags.items():
 		if value != '':
-			if key not in ('trailid', 'systemname', 'mtr_vhcle', 'cnstrctn', 'hwy_abndnd'):
-				newtags[key] = value
+			if key == 'trailid':
+				newtags['RLIS:trailid'] = value
+			elif key == 'systemname':
+				newtags['RLIS:systemname'] = value
+			elif key == 'mtr_vhcle':
+				newtags['motor_vehicle'] = value
+			elif key == 'cnstrctn':
+				newtags['construction'] = value
+			elif key == 'hwy_abndnd':
+				newtags['highway:abandoned'] = value
 			else:
-				if key == 'trailid':
-					newtags['RLIS:trailid'] = value
-				elif key == 'systemname':
-					newtags['RLIS:systemname'] = value
-				elif key == 'mtr_vhcle':
-					newtags['motor_vehicle'] = value
-				elif key == 'cnstrctn':
-					newtags['construction'] = value
-				elif key == 'hwy_abndnd':
-					newtags['highway:abandoned'] = value
+				newtags[key] = value
+	
 	return newtags
