ccd52bb0df02f9786fb2ec868831d363604c4222
grant-humphries <grant.humphries@gmail.com>
2014-12-02 16:21:16 -0800

    more improvements to titlecase function

--- postgis/string2titlecase.sql
+++ postgis/string2titlecase.sql
@@ -7,62 +7,72 @@
 --http://archives.postgresql.org/pgsql-sql/2010-09/msg00088.php
 --modified to suit this project by Grant Humphries
 
-create or replace function "format_titlecase" ("v_inputstring" varchar)
-returns varchar as
+create or replace function "format_titlecase" ("v_inputstring" text)
+returns text as
 $body$
 
 declare
-	v_Index int;
-	v_Char char(1);
-	v_OutputString varchar(4000);
-	SWV_InputString varchar(4000);
+	ix int;
+	curChar char(1);
+	outString text;
+	inString text;
 
 begin
-	SWV_InputString := v_InputString;
+	inString := v_InputString;
 	--cures problem where string starts with blank space and removes back-to-back spaces
-	SWV_InputString := regexp_replace(ltrim(rtrim(SWV_InputString)), '\s+', '\s'); 
-	v_OutputString := lower(SWV_InputString);
-	v_Index := 1;
+	inString := regexp_replace(ltrim(rtrim(inString)), '\s{2,}', ' ', 'g'); 
+	outString := lower(inString);
+	ix := 1;
 	--replaces 1st char of Output with uppercase of 1st char from input
-	v_OutputString := overlay(v_OutputString placing upper(substr(SWV_InputString, 1, 1)) from 1 for 1); 
+	outString := overlay(outString placing upper(substr(inString, 1, 1)) from 1 for 1); 
 	
-	while v_Index <= length(SWV_InputString) loop
-		v_Char := substr(SWV_InputString, v_Index, 1); 
+	while ix <= length(inString) loop
+		curChar := substr(inString, ix, 1); 
 		--gets loop's working character
-		if v_Char in ('m','M',' ',';',':','!','?',',','.','_','-','/','&','''','(',chr(9),'0','1','2','3','4','5','6','7','8','9') then
-			if v_Index+1 <= length(SWV_InputString) then
-				--if the working char is an apostrophe and the letter after that is not S
-				if v_Char = '''' and upper(substr(SWV_InputString,v_Index+1,1)) <> 'S' and substr(SWV_InputString,v_Index+2,1) <> repeat(' ',1) then
-					v_OutputString := overlay(v_OutputString placing upper(substr(SWV_InputString,v_Index+1,1)) from v_Index+1 for 1);
-				--if the working char is an &
-				elsif v_Char = '&' then
-					if(substr(SWV_InputString,v_Index+1,1)) = ' ' then
-						v_OutputString := overlay(v_OutputString placing upper(substr(SWV_InputString,v_Index+2,1)) from v_Index+2 for 1);
-					else
-						v_OutputString := overlay(v_OutputString placing upper(substr(SWV_InputString,v_Index+1,1)) from v_Index+1 for 1);
-					end if;
-				--if working character is not 'm', in this case I want single letter words to be capitalized
-				--if that's not the case add the clause that the second character after the working character 
-				--cannot be a space
-				elsif upper(v_Char) != 'M' and (substr(SWV_InputString,v_Index+1,1) <> repeat(' ',1)) then
-					v_OutputString := overlay(v_OutputString placing upper(substr(SWV_InputString,v_Index+1,1)) from v_Index+1 for 1);
-				--special case for handling 'Mc' as in McDonald
-				elsif upper(v_Char) = 'M' and upper(substr(SWV_InputString,v_Index+1,1)) = 'C' and (substring(SWV_InputString,v_Index-1,1) in (' ','-') or v_Index=1) then
-					v_OutputString := overlay(v_OutputString placing upper(substr(SWV_InputString,v_Index,1)) from v_Index for 1);
-					--makes the 'C' lower case.
-					v_OutputString := overlay(v_OutputString placing lower(substr(SWV_InputString,v_Index+1,1)) from v_Index+1 for 1);
-					--makes the letter after the 'c' upper case
-					v_OutputString := overlay(v_OutputString placing upper(substr(SWV_InputString,v_Index+2,1)) from v_Index+2 for 1);
-					--we took care of the char acfter 'c' (we handled 2 letters instead of only 1 as usual), so we need to advance.
-					v_Index := v_Index+1;
+		if ix+1 <= length(inString) then
+			--if the working character is an apostrophe and there is not a dash, 
+			--space or end of line two characters later
+			if curChar = '''' and substr(inString, ix+2, 1) !~ '(-|\s)' 
+				and ix+2 != length(inString) then
+					outString := overlay(outString placing 
+					upper(substr(inString, ix+1, 1)) from ix+1 for 1);
+			--if the working char is an &
+			elsif curChar = '&' then
+				if(substr(inString, ix+1, 1)) = ' ' then
+					outString := overlay(outString placing 
+					upper(substr(inString, ix+2, 1)) from ix+2 for 1);
+				else
+					outString := overlay(outString placing 
+					upper(substr(inString, ix+1, 1)) from ix+1 for 1);
 				end if;
+			--special case for handling 'Mc' as in McDonald
+			elsif lower(curChar) = 'm' and lower(substr(inString, ix+1, 1)) = 'c' 
+				and (substring(inString, ix-1, 1) ~ '(-|\s)' or ix=1) then
+					outString := overlay(outString placing 
+					upper(substr(inString, ix, 1)) from ix for 1);
+					--makes the 'C' lower case.
+					outString := overlay(outString 
+					placing lower(substr(inString, ix+1, 1)) from ix+1 for 1);
+					--makes the letter after the 'c' upper case
+					outString := overlay(outString placing 
+					upper(substr(inString, ix+2, 1)) from ix+2 for 1);
+					--we took care of the char acfter 'c' (we handled 2 letters instead 
+					--of only 1 as usual), so we need to advance.
+					ix := ix+1;
+			--capitalize letters that follow all of these characters, barring special cases 
+			--identified below
+			elsif (curChar in (' ',';',':','!','?',',','.','_','-','/','(',chr(9)) 
+				and substr(inString, ix+1, 1) !~ '(-|\s)') or (curChar ~ '[0-9]' 
+				and lower(substring(inString, ix+1, 2)) !~ '(st|nd|rd|th)') then
+					outString := overlay(outString placing 
+					upper(substr(inString, ix+1, 1)) from ix+1 for 1);
 			end if;
 		end if;
 
-		v_Index := v_Index+1;
+		ix := ix+1;
 	end loop; 
 
-	return coalesce(v_OutputString,'');
+	return coalesce(outString,'');
 end;
 $body$
 language 'plpgsql'
