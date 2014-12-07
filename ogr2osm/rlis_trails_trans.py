ce47d968c8eda27098d4b7c86b1aa1a2c592a9b3
grant-humphries <grant.humphries@gmail.com>
2014-12-06 16:25:13 -0800

    added function description

--- ogr2osm/rlis_trails_trans.py
+++ ogr2osm/rlis_trails_trans.py
@@ -1,9 +1,11 @@
 # Python Version: 2.7.5
 #--------------------------------
-
 # In this case the translation file is being used to expand fields (keys) that
 # had to be truncated due to the .dbf spec associated with shapefiles
+
 def filterTags(tags):
+	"""This function reads and potentially modifies key-value pairs, if a key has no value,
+	which is common in may geoformats, it will be dropped"""
 	if tags is None:
 		return
 	newtags = {}
