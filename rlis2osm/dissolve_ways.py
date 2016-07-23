5a030bfcb0e79fabc58b5608a2da4d7c8e2b9133
grant-humphries <grant.humphries@gmail.com>
2016-07-23 12:57:20 -0700

    started implementing object oriented approach, provided sup port for both zipped and unzipped shapefiles

--- rlis2osm/dissolve_ways.py
+++ rlis2osm/dissolve_ways.py
@@ -1,4 +1,5 @@
-from os.path import abspath, dirname, exists, join
+import logging
+from os.path import abspath, basename, dirname, exists, join
 from collections import defaultdict
 
 import fiona
@@ -10,7 +11,7 @@ RLIS_URL = 'http://library.oregonmetro.gov/rlisdiscovery'
 STREETS = 'streets.zip'
 TRAILS = 'trails.zip'
 RLIS_TERMS = 'http://rlisdiscovery.oregonmetro.gov/view/terms.htm'
-TEST_WAYS = join(dirname(abspath(__name__)), 'data', STREETS)
+TEST_WAYS = join(dirname(abspath(__name__)), 'data', 'fwy.zip')
 
 
 def download_rlis():
@@ -25,49 +26,125 @@ def get_data_paths():
         streets_shp = join(dirname(abspath(__name__)), )
 
 
-def dissolve_ways(dissolve_fields=None):
-    ways = fiona.open('/streets.shp', vfs=TEST_WAYS)
-    print ways[0]['properties']
-    # map_end_points_to_ways(ways)
+class WayDissolver(object):
 
-    ways.close()
+    def __init__(self, way_path, tags=None, tag_exclude=True):
+        self.way_path = way_path
+        self.ways = self.open_ways()
+        self.tags = self._define_filter_tags(tags, tag_exclude)
 
-    # print ways[0]['properties']
-    # exit()
+    def open_ways(self):
+        """fiona has the ability open zipped shapefiles, this method
+        handles both zipped and unzipped
+        """
 
-    # assigned = list()
-    # for node_id, way_list in node_way_map.items():
+        file_ext = self.way_path.split('.')[-1]
+        if file_ext == 'zip':
+            vfs = 'zip:///{}'.format(self.way_path)
+            path = '/{}'.format(
+                basename(self.way_path).replace('.zip', '.shp'))
+        else:
+            vfs = None
+            path = self.way_path
+
+        ways = fiona.open(path, vfs=vfs)
+        return ways
+
+    def close_ways(self):
+        """ways should be closed when processing is done"""
+
+        self.ways.close()
+
+    def _define_filter_tags(self, tags, tag_exclude):
+        """verify that supplied fields exist in the shapefile and define
+        the fields that must match for a merge to be allowed
+        """
+
+        fields = self.ways[0]['properties'].keys()
+        if tags:
+            for t in tags:
+                if t not in fields:
+                    logging.error('supplied field: "{}", does not exists in '
+                                  'the data, modify the "tags" input and run '
+                                  'again'.format(t))
+                    exit()
+            if tag_exclude:
+                for t in tags:
+                    fields.pop(t)
+            else:
+                fields = tags
+
+        return fields
+
+    def dissolve_ways(self):
+        node_way_map, way_nodes = self._map_end_pts_to_ways()
+
+        assigned = list()
+        way_groups = list()
+        for fid, feat in self.ways.items():
+            if fid in assigned:
+                continue
+
+            group = list(fid)
+            nodes = way_nodes[fid].values()
+            tags = feat['properties']
+
+            while nodes:
+                n = nodes.pop()
+                connected_ways = node_way_map[n]
+                connected_ways.pop(n)
+
+                for way_id in connected_ways:
+                    if way_id in assigned:
+                        continue
+
+                    if feat['properties'] == self.ways[way_id]['properties']:
+                        this_group.add(way_id)
+                        for new_node in way_nodes[fid].values():
+                            if new_node != n:
+                                nodes.append(n)
+                print nodes
+            exit()
+
+    def _map_end_pts_to_ways(self):
+        node_counter = 0
+        node_id_map = dict()
+        node_way_map = defaultdict(list)
+        way_nodes = dict()
+
+        for fid, feat in self.ways.items():
+            geom = shape(feat['geometry'])
+            coords = list(geom.coords)
+            f_node = coords[0]
+            t_node = coords[-1]
+
+            for node in (f_node, t_node):
+                if node not in node_id_map:
+                    node_id_map[node] = node_counter
+                    node_counter += 1
+
+                node_id = node_id_map[node]
+                node_way_map[node_id].append(fid)
+
+            way_nodes[fid] = {
+                'f': node_id_map[f_node],
+                't': node_id_map[t_node]
+            }
+
+        return node_way_map, way_nodes
 
 
-def map_end_points_to_ways(ways):
-    node_counter = 0
-    node_id_map = dict()
-    node_way_map = defaultdict(list)
+def main():
+    base_path = join(dirname(abspath(__name__)), 'data')
+    zip_path = join(base_path, 'fwy.zip')
+    shp_path = join(base_path, 'unzipped', 'fwy.shp')
 
-    for fid, feat in ways.items():
-        geom = shape(feat['geometry'])
-        coords = list(geom.coords)
-        f_node = coords[0]
-        t_node = coords[-1]
+    zip_dissolver = WayDissolver(zip_path)
+    shp_dissolver = WayDissolver(shp_path)
 
-        for node in (f_node, t_node):
-            if node not in node_id_map:
-                node_id_map[node] = node_counter
-                node_counter += 1
-
-            node_id = node_id_map[node]
-            node_way_map[node_id].append(fid)
-
-        fields = feat['properties']
-        fields['f_node'] = node_id_map[f_node]
-        fields['t_node'] = node_id_map[t_node]
-
-    # return ways, node_way_map
-
-    for fid, feat in ways.items():
-        print fid, feat['properties']
-        exit()
+    zip_dissolver.close_ways()
+    shp_dissolver.close_ways()
 
 
 if __name__ == '__main__':
-    dissolve_ways()
+    main()
