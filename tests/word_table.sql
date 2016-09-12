1507ad5fa4703f181ac2ff08a2e5c6f663f50086
grant-humphries <grant.humphries@gmail.com>
2016-09-12 11:29:17 -0700

    reworked word split script to split at dashes and forward slashes and to track the words position in the original name, resolves #3

--- tests/word_table.sql
+++ tests/word_table.sql
@@ -2,34 +2,43 @@
 --entries in a table
 drop table if exists words cascade;
 create table words as
-    select distinct
-        regexp_split_to_table(streetname, E'\\s+')::text as word,
-        'streetname'::text as field, streetname as fullname
-    from streets
-        union all
-    select distinct
-        regexp_split_to_table(trailname, E'\\s+'), 'trailname',
-        trailname
-    from trails
-        union all
-    select distinct
-        regexp_split_to_table(sharedname, E'\\s+'), 'sharedname',
-        sharedname
-    from trails
-        union all
-    select distinct
-        regexp_split_to_table(systemname, E'\\s+') , 'systemname',
-        systemname
-    from trails
-        union all
-    select distinct
-        regexp_split_to_table(agencyname, E'\\s+'), 'agencyname',
-        agencyname
-    from trails;
+    --split names at dashes and combine them into a single table, with
+    --record of position in the original string
+    with dash_split as (
+        select distinct
+            phrase, streetname as fullname, delimiter_pos,
+            'streetname'::text as field
+        from streets, regexp_split_to_table(streetname, '\s*-+\s*')
+            with ordinality x(phrase, delimiter_pos)
+                union all
+        select distinct
+            phrase, trailname, delimiter_pos, 'trailname'
+        from trails, regexp_split_to_table(trailname, '\s*-+\s*')
+            with ordinality x(phrase, delimiter_pos)
+                union all
+        select distinct
+            phrase, sharedname, delimiter_pos, 'sharedname'
+        from trails, regexp_split_to_table(sharedname, '\s*-+\s*')
+            with ordinality x(phrase, delimiter_pos)
+                union all
+        select distinct
+            phrase, systemname, delimiter_pos, 'systemname'
+        from trails, regexp_split_to_table(systemname, '\s*-+\s*')
+            with ordinality x(phrase, delimiter_pos)
+                union all
+        select distinct
+            phrase, agencyname , delimiter_pos, 'agencyname'
+        from trails, regexp_split_to_table(agencyname, '\s*-+\s*')
+            with ordinality x(phrase, delimiter_pos))
 
-alter table words add len int;
-update words set len = char_length(word);
+    --split phrases at spaces and forward slash with record of each
+    --word position in the phrase
+    select
+        word, fullname, separator_pos, delimiter_pos,
+        char_length(word) as word_length, field
+    from dash_split, regexp_split_to_table(phrase, '[\s/]+')
+        with ordinality x(word, separator_pos);
 
 create index word_ix on words using BTREE (word);
 create index field_ix on words using BTREE (field);
-create index len_ix on words using BTREE (len);
+create index len_ix on words using BTREE (word_length);
